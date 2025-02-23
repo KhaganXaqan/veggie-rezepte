e2c4641f2da511df00b6e0f3f707e90f2b130bc9
Farid <farid.muradov@yieldlove.com>
2025-02-23 23:59:11 +0100

    8

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: { 
-    domains: ['images.unsplash.com'],
-    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
