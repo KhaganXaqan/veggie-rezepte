7926284ee4a91d3b93ae9dde0e739e63aaee17b9
Khagan Rahimzade <khagan.rahimzade@protonmail.com>
2025-02-27 01:23:37 +0100

    fixed warnings "during build"

--- next.config.js
+++ next.config.js
@@ -13,32 +13,7 @@ const nextConfig = {
     optimizeCss: true,
     optimizePackageImports: ['lucide-react'],
   },
-  output: 'export',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin'
-          }
-        ],
-      },
-    ]
-  },
+  output: 'export'
 };
 
 module.exports = nextConfig;
