ae2d0aa378404c6706b490c13b74563801ca3219
Khagan Rahimzade <khagan.rahimzade@protonmail.com>
2025-06-05 22:57:23 +0200

    fixed canonical urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { recipes } from '@/lib/data'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://veggie-rezepte.de'
+  const baseUrl = 'https://www.veggie-rezepte.de'
 
   // Base routes
   const routes = [
