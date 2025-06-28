59545c696ca8fe780ec6469247df9d02da8d8c7e
Khagan55 <84350970+Khagan55@users.noreply.github.com>
2025-06-28 20:44:57 +0200

    Critical Project Improvements

--- app/robots.ts
+++ app/robots.ts
@@ -4,14 +4,22 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://veggie-rezepte.de'
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/*', '/admin/*'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/*', '/admin/*', '/_next/*', '/private/*'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/*', '/admin/*'],
+      }
+    ],
     sitemap: [
       `${baseUrl}/sitemap.xml`,
       `${baseUrl}/imageSitemap.xml`
     ],
+    host: baseUrl,
   }
 }
